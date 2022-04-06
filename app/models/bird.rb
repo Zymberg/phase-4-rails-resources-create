ae76c3cc1dd0b9bd4da7225e97de5a06b60b72db
Granit <gzymeri55@gmail.com>
2022-04-06 20:21:28 +0200

    completed

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,15 @@
 class Bird < ApplicationRecord
+
+    def create
+        bird = Bird.create(name: params[:name], species: params[:species])
+        render json: bird, status: :created
+      end
+
+      # GET /birds/:id
+    def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
 end
